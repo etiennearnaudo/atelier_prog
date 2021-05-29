28f62d25f64578f302fdf925d79180f533e36414
Pierre-Marie CIRON <pierre-marie.ciron@eleves.enpc.fr>
2021-05-29 16:02:29 +0200

    Update squelette pima

--- mario.cpp
+++ mario.cpp
@@ -28,19 +28,3 @@ int main()
     return 0;
 
 }
-
-///Test 3D
-//    openWindow3D(largeur*taille_case, hauteur*taille_case);
-
-//    Mesh face_gauche=Mesh::Plane(DoublePoint3(0,int(hauteur*taille_case/2),int(taille_case/2)),DoubleVector3(0,hauteur*taille_case,0),DoubleVector3(0,0,taille_case)); //Face gauche
-//    Mesh face_droite=Mesh::Plane(DoublePoint3(largeur*taille_case,int(hauteur*taille_case/2),int(taille_case/2)),DoubleVector3(0,hauteur*taille_case,0),DoubleVector3(0,0,taille_case));
-//    Mesh face_haut=Mesh::Plane(DoublePoint3(int(largeur*taille_case/2),hauteur*taille_case,int(taille_case/2)),DoubleVector3(largeur*taille_case,0,0),DoubleVector3(0,0,taille_case));
-//    Mesh face_bas=Mesh::Plane(DoublePoint3(int(largeur*taille_case/2),0,int(taille_case/2)),DoubleVector3(largeur*taille_case,0,0),DoubleVector3(0,0,taille_case));
-//    showMesh(face_gauche);
-//    showMesh(face_droite);
-//    showMesh(face_bas);
-//    showMesh(face_haut);
-
-//    setCamera(DoublePoint3(int(largeur*taille_case/2),int(hauteur*taille_case/2),0),DoublePoint3(0,0,taille_case),DoubleVector3(0,1,0));
-
-//    endGraphics();
